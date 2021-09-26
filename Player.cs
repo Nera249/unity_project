f81298bd75ba44c68d18feb5e78e856912053c45
Le Flem Paul <paul.leflem@icloud.com>
2021-09-26 18:09:45 +0200

    changes

--- Player.cs
+++ Player.cs
@@ -4,34 +4,96 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{  
-    
-   private bool JumpKeyPressed;
-   private float HorizontalInput;
-   
-    // Start is called before the first frame update
-    void Start()
-        {
+{
+    private bool JumpKeyPressed;
+    private bool ZKeyPressed;
+    private bool QKeyPressed;
+    private bool SKeyPressed;
+    private bool DKeyPressed;
 
-        }
-
-    // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))                //Debug.Log("Space Key Pressed");           
         {
-            if (Input.GetKeyDown(KeyCode.Space))                //Debug.Log("Space Key Pressed");           
-                {
-                   JumpKeyPressed = true;                    
-                }
-    
+            JumpKeyPressed = true;
+        } else {
+            JumpKeyPressed = false;
         }
-    
+
+        if (Input.GetKeyDown(KeyCode.Z))                           
+        {
+            ZKeyPressed = true;
+        } else {
+            ZKeyPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))                           
+        {
+            QKeyPressed = true;
+        } else {
+            QKeyPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))                           
+        {
+            SKeyPressed = true;
+        } else {
+            SKeyPressed = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))                           
+        {
+            DKeyPressed = true;
+        } else {
+            DKeyPressed = false;
+        }
+
+    }
+
     // FixedUpdate is called once every phisyc update 
-    private void FixedUpdate()          
+    private void FixedUpdate()
+    {
+        if (JumpKeyPressed)                //Debug.Log("Space Key Pressed");           
         {
-             if (JumpKeyPressed)                //Debug.Log("Space Key Pressed");           
-                {
-                    GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);  
-                    JumpKeyPressed = false;                    
-                }   
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            JumpKeyPressed = false;
         }
+        if (ZKeyPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * 1, ForceMode.VelocityChange);
+
+            if (Input.GetKeyUp(KeyCode.Z))
+            {
+                ZKeyPressed = false;
+            }
+        }
+        if (QKeyPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.left * 1, ForceMode.VelocityChange);
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                QKeyPressed = false;
+            }
+        }
+        if (SKeyPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.back * 1, ForceMode.VelocityChange);
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                SKeyPressed = false;
+            }
+        }
+        if (DKeyPressed)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * 1, ForceMode.VelocityChange);
+
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                DKeyPressed = false;
+            }
+        }
+
+    }
 }
